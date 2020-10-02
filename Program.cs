97b1eda3d3858a83bbdfaff89f45a2e3034a7804
PhilipHadland <71390387+PhilipHadland@users.noreply.github.com>
2020-10-02 14:15:21 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,41 @@
+sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NumberConverter x = new NumberConverter();
+            x.ConvertNumber(5);
+            x.ConvertNumber(15);
+            x.ConvertNumber(50);
+            x.ConvertNumber(99);
+            x.ConvertNumber(100);
+            x.ConvertNumber(900);
+            x.ConvertNumber(770);
+            x.ConvertNumber(170);
+            x.ConvertNumber(990);
+            x.ConvertNumber(333);
+            x.ConvertNumber(753);
+            x.ConvertNumber(192);
+            x.ConvertNumber(999);
+            x.ConvertNumber(100);
+            x.ConvertNumber(109);
+            x.ConvertNumber(110);
+            x.ConvertNumber(200);
+            x.ConvertNumber(911);
+            x.ConvertNumber(9999);
+            x.ConvertNumber(1234);
+            x.ConvertNumber(1516);
+
+
+            Console.Read();
+        }
+    }
+}
